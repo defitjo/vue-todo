cd575b5c2a1c42f8ea2103bd3d9454416bc88351
defitjo <defitjo16@gmail.com>
2019-01-26 03:11:35 -0500

    added buttons

--- vue-todo/src/views/Home.vue
+++ vue-todo/src/views/Home.vue
@@ -3,13 +3,49 @@
     <v-container class="my-5">
       <v-card flat>
         <v-layout pa-4 row wrap>
-          <v-flex xs12 md5>
+          <v-flex xs12 md6>
             <div class="caption">Todo Title</div>
             <div>Testing Todo</div>
           </v-flex>
-          <v-flex xs12 md5>
+          <v-flex xs12 md4>
             <div class="caption">Due Date</div>
             <div>February 1, 2018</div>
+          </v-flex>
+          <v-flex xs12 md2>
+            <div class="caption ml-2">Todo Status</div>
+            <div>
+              <v-btn small flat icon color="success">
+                <v-icon>check</v-icon>
+              </v-btn>
+              <v-btn small flat icon color="warning">
+                <v-icon>close</v-icon>
+              </v-btn>
+            </div>
+          </v-flex>
+        </v-layout>
+        <v-divider></v-divider>
+      </v-card>
+
+      <v-card flat>
+        <v-layout pa-4 row wrap>
+          <v-flex xs12 md6>
+            <div class="caption">Todo Title</div>
+            <div>Testing Todo 2</div>
+          </v-flex>
+          <v-flex xs12 md4>
+            <div class="caption">Due Date</div>
+            <div>February 10, 2018</div>
+          </v-flex>
+          <v-flex xs12 md2>
+            <div class="caption ml-2">Todo Status</div>
+            <div>
+              <v-btn small flat icon color="success">
+                <v-icon>check</v-icon>
+              </v-btn>
+              <v-btn small flat icon color="warning">
+                <v-icon>close</v-icon>
+              </v-btn>
+            </div>
           </v-flex>
         </v-layout>
         <v-divider></v-divider>
@@ -21,6 +57,10 @@
 <script>
 
   export default {
+    data () {
+      return {
 
+      }
+    }
   }
 </script>

f45b9e5ca34ae0aa06e66ac24a2c2913679b7cd6
defitjo <defitjo16@gmail.com>
2019-01-30 21:46:01 -0500

    created todo form

--- vue-todo/src/views/Home.vue
+++ vue-todo/src/views/Home.vue
@@ -62,6 +62,25 @@
         </v-layout>
         <v-divider></v-divider>
       </v-card>
+
+      <v-dialog v-model="dialog" persistent max-width="600px">
+        <v-btn slot="activator" color="primary" dark>Add New Todo</v-btn>
+        <v-card>
+          <v-card-title>
+            <h3>Add New Todo</h3>
+          </v-card-title>
+          <v-form class="px-4" ref="form">
+            <v-text-field v-model="title" label="Todo Title" :rules="titleRules" prepend-icon="title"></v-text-field>
+            <v-textarea v-model="info" label="Todo Info" :rules="infoRules" prepend-icon="edit"></v-textarea>
+            <v-menu v-model="menu" :close-on-content-click="false">
+              <v-text-field slot="activator" :rules="dateRules" clearable label="Due Date" prepend-icon="date_range"></v-text-field>
+            </v-menu>
+          </v-form>
+          <v-spacer></v-spacer>
+          <v-btn class="success" flat @click="submit">Add Todo</v-btn>
+          <v-btn color="white" class="blue darken-1" flat @click="dialog = false">Close</v-btn>
+        </v-card>
+      </v-dialog>
     </v-container>
   </div>
 </template>
@@ -71,7 +90,7 @@
   export default {
     data () {
       return {
-
+        dialog: false
       }
     }
   }
